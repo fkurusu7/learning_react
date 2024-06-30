e96e24d5efb9749b9ecbdd8733fa7bfa8680a2c1
Fernando Cruz <f.douzez@gmail.com>
2024-06-29 23:30:05 -0600

    add localstorage with useEffect and useState

--- src/apps/usepopcorn/UsePopcornApp.jsx
+++ src/apps/usepopcorn/UsePopcornApp.jsx
@@ -65,10 +65,15 @@ const URL_MOVIE_API = `${URL_BASE_MOVIE_API}${KEY}`;
 export default function UsePopcornApp() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -80,12 +85,18 @@ export default function UsePopcornApp() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
-    localStorage.setItem("watched", [...watched]);
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
